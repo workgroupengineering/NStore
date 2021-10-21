24ca3156e24f39584de3ec253f9c104e705ef14d
Gian Maria Ricci <alkampfer@nablasoft.com>
2021-10-21 19:27:37 +0200

    Starting of fast method invoker.

---
+++ src/NStore.Core.Tests/Processing/FastMethodInvokerTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using NStore.Core.Processing;
+using Xunit;
+
+namespace NStore.Core.Tests.Processing
+{
+    public class FastMethodInvokerTests
+    {
+        private readonly Target _target = new Target();
+
+        [Fact]
+        public void invoker_correctly_invoke_method()
+        {
+            FastMethodInvoker.CallPublicReturningVoid(_target, "DoSomething", "hello");
+            Assert.Equal("hello", _target.Param);
+        }
+
+        [Fact]
+        public void invoker_correctly_invoke_method_with_return_type()
+        {
+            var result = (string)FastMethodInvoker.CallPublic(_target, "DoSomethingReturn", "hello");
+            Assert.Equal("hello", _target.Param);
+            Assert.Equal("processed hello", result);
+        }
+
+        [Fact]
+        public void invoker_correctly_dispatch_to_object()
+        {
+            var result = (string)FastMethodInvoker.CallPublic(_target, "DoSomethingWithObjectReturn", new object());
+            Assert.Equal("processed", result);
+        }
+
+        [Fact]
+        public void invoker_on_optional_public_method_should_not_mask_exception()
+        {
+            Assert.Throws<TargetException>(() =>
+                FastMethodInvoker.CallPublicReturningVoid(_target, "FailPublic", new object())
+            );
+        }
+
+        [Fact]
+        public void invoker_on_private_method_should_not_mask_exception()
+        {
+            Assert.Throws<TargetException>(() =>
+                FastMethodInvoker.CallNonPublicIfExists(_target, "FailPrivate", new object())
+            );
+        }
+
+        [Fact]
+        public void invoker_on_private_methods_should_not_mask_exception()
+        {
+            Assert.Throws<TargetException>(() =>
+                FastMethodInvoker.CallNonPublicIfExists(_target, new[] { "FailPrivate", "FailPrivate" }, new object())
+            );
+        }
+    }
+}
---
+++ src/NStore.Core/Processing/FastMethodInvoker.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace NStore.Core.Processing
+{
+    public static class FastMethodInvoker
+    {
+        static BindingFlags NonPublic = BindingFlags.NonPublic | BindingFlags.Instance;
+        static BindingFlags Public = BindingFlags.Public | BindingFlags.Instance;
+
+        public static object CallNonPublicIfExists(object instance, string methodName, object @parameter)
+        {
+            var cacheKey = $"{instance.GetType().FullName}/{methodName}";
+            if (!lcgCacheFunction.TryGetValue(cacheKey, out var caller))
+            {
+                var mi = instance.GetType().GetMethod(
+                    methodName,
+                    NonPublic,
+                    null,
+                    new Type[] { @parameter.GetType() },
+                    null
+                );
+
+                caller = ReflectFunction(instance.GetType(), mi);
+
+                lcgCacheFunction[cacheKey] = caller;
+            }
+            return caller(instance, @parameter);
+        }
+
+        public static object CallNonPublicIfExists(object instance, string[] methodNames, object @parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static object CallPublic(object instance, string methodName, object @parameter)
+        {
+            var cacheKey = $"{instance.GetType().FullName}/{methodName}";
+            if (!lcgCacheFunction.TryGetValue(cacheKey, out var caller))
+            {
+                var mi = instance.GetType().GetMethod(
+                    methodName,
+                    Public,
+                    null,
+                    new Type[] { @parameter.GetType() },
+                    null
+                );
+
+                caller = ReflectFunction(instance.GetType(), mi);
+
+                lcgCacheFunction[cacheKey] = caller;
+            }
+            return caller(instance, @parameter);
+        }
+
+        public static void CallPublicReturningVoid(object instance, string methodName, object @parameter)
+        {
+            var cacheKey = $"{instance.GetType().FullName}/{methodName}";
+            if (!lcgCache.TryGetValue(cacheKey, out var caller))
+            {
+                var mi = instance.GetType().GetMethod(
+                    methodName,
+                    Public,
+                    null,
+                    new Type[] { @parameter.GetType() },
+                    null
+                );
+
+                caller = ReflectAction(instance.GetType(), mi);
+
+                lcgCache[cacheKey] = caller;
+            }
+            caller(instance, @parameter);
+        }
+
+        /// <summary>
+        /// Cache of appliers, for each domain object I have a dictionary of actions
+        /// </summary>
+        private static ConcurrentDictionary<string, Action<Object, Object>> lcgCache = new ConcurrentDictionary<string, Action<object, object>>();
+        private static ConcurrentDictionary<string, Func<Object, Object, Object>> lcgCacheFunction = new ConcurrentDictionary<string, Func<Object, Object, Object>>();
+
+        private class ObjectApplier
+        {
+            private Dictionary<Type, RuntimeMethodHandle> appliers =
+                new Dictionary<Type, RuntimeMethodHandle>();
+        }
+
+        public static Action<Object, Object> ReflectAction(Type objType, MethodInfo methodinfo)
+        {
+            DynamicMethod retmethod = new DynamicMethod(
+                "Invoker" + methodinfo.Name,
+                (Type)null,
+                new Type[] { typeof(Object), typeof(Object) },
+                objType,
+                true); //methodinfo.GetParameters().Single().ParameterType
+            ILGenerator ilgen = retmethod.GetILGenerator();
+            ilgen.Emit(OpCodes.Ldarg_0);
+            ilgen.Emit(OpCodes.Castclass, objType);
+            ilgen.Emit(OpCodes.Ldarg_1);
+            ilgen.Emit(OpCodes.Callvirt, methodinfo);
+            ilgen.Emit(OpCodes.Ret);
+            return (Action<Object, Object>)retmethod.CreateDelegate(typeof(Action<Object, Object>));
+        }
+
+        public static Func<Object, Object, Object> ReflectFunction(Type objType, MethodInfo methodinfo)
+        {
+            DynamicMethod retmethod = new DynamicMethod(
+                "Invoker" + methodinfo.Name,
+                typeof(Object),
+                new Type[] { typeof(Object), typeof(Object) },
+                objType,
+                true); //methodinfo.GetParameters().Single().ParameterType
+            ILGenerator ilgen = retmethod.GetILGenerator();
+            ilgen.Emit(OpCodes.Ldarg_0);
+            ilgen.Emit(OpCodes.Castclass, objType);
+            ilgen.Emit(OpCodes.Ldarg_1);
+            ilgen.Emit(OpCodes.Callvirt, methodinfo);
+            ilgen.Emit(OpCodes.Ret);
+            return (Func<Object, Object, Object>)retmethod.CreateDelegate(typeof(Func<Object, Object, Object>));
+        }
+    }
+}
